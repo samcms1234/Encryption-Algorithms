dd179f168a0a1d090e5f9b5fbaee5c469e02842a
samcms1234 <samdevpro99@gmail.com>
2022-02-06 17:27:44 +0530

    Added the code for General caesar algo

--- Substitution-cipher/General-Caesar-cipher.py
+++ Substitution-cipher/General-Caesar-cipher.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
 
+def Caesar_cipher_general(text,shift) -> str:
+    Digest=""
+    for i in text:
+        if i.isupper():
+            Digest+=chr(((ord(i)+shift-65)%26)+65)
+        elif i.islower():
+             Digest+=chr(((ord(i)+shift-97)%26)+97)
+        else:
+            Digest+=i
+    return Digest
+if __name__=="__main__":
+    PlainText=input("Enter Plain Text: ")
+    shift=int(input("Enter Shift: "))
+    print(Caesar_cipher_general(PlainText,shift))
+    
